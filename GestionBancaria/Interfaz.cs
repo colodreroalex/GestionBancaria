0e357bacbd46f41401b66686baf8916922cc21a4
alejandro <garciacolodreroalex@outlook.com>
2023-04-14 08:21:29 +0200

    Agregar archivos de proyecto.

---
+++ GestionBancaria/Interfaz.cs
@@ -0,0 +1,166 @@
+﻿using BGCuentas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GestionBancaria
+{
+    internal class Interfaz
+    {
+        /// <summary>
+        /// Menu principal de la Aplicacion
+        /// </summary>
+        public static void MenuPrincipal()
+        {
+            //Aqui no valido ninguna opcion ya que el menu lo mostrare en mas sitios , por eso
+            //creare un metodo solo de validacion
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine($"\t0.- Salir");
+            Console.WriteLine($"\t1.- Agregar Cuenta");
+            Console.WriteLine($"\t2.- Modificar Cuenta");
+            Console.WriteLine($"\t3.- Eliminar Cuenta");
+            Console.WriteLine($"\t4.- Consultar");
+            Console.WriteLine($"\t5.- Operar");
+            Console.WriteLine($"\t6.- Cargar Datos\n");
+
+        }
+
+        public static void MenuSecundario()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            //Console.WriteLine("\t1.-Cuenta General");
+            Console.WriteLine("\t0.-Salir");
+            Console.WriteLine("\t1.-Cuenta Joven");
+            Console.WriteLine("\t2.-Cuenta Oro");
+            Console.WriteLine("\t3.-Cuenta Platino\n");
+        }
+
+        public static void MenuOperaciones()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("\t0.-Salir");
+            Console.WriteLine("\t1.-Ingresar");
+            Console.WriteLine("\t2.-Retirar");
+        }
+
+        public static byte LeerOpcion(byte opMin, byte opMax)
+        {
+            byte op;
+
+            Console.Write("\tIntroduzca su elección: ");
+            op = Convert.ToByte(Console.ReadLine());
+
+            if (op < opMin) throw new Exception($"Opcion {op} no válida");
+            if (op > opMax) throw new Exception($"Opcion {op} no válida");
+
+            return op;
+        }
+
+        public static void InformarError(string message)
+        {
+            Console.WriteLine("\nSE HA PRODUCIDO UN ERROR");
+            Console.WriteLine($"ERROR: {message}");
+            Console.WriteLine("Pulse ENTER para continuar...");
+            Console.ReadLine(); //Vaciar buffer del teclado 
+        }
+
+        
+
+        public static Opcion OpcionDelMenuPrincipal()
+        {
+            //RECURSOS
+            Opcion opcion = Opcion.Cargar;  //Inicializacion por ejemplo
+            bool opcionCorrecta;
+
+
+            do
+            {
+                opcionCorrecta = false;
+
+                //Mostrar el MENU PRINCIPAL
+                Interfaz.MenuPrincipal();
+
+                //SELECCIONAR OPCION
+                try
+                {
+                    opcion = (Opcion)Interfaz.LeerOpcion((byte)Opcion.Salir, (byte)Opcion.Cargar);
+                    opcionCorrecta = true;
+
+                }
+                catch (Exception ex) //Se podrian generar excepciones personalizadas
+                {
+                    Interfaz.InformarError(ex.Message);
+
+                }
+
+            } while (!opcionCorrecta);
+
+
+
+            return opcion;
+        }
+
+        public static AgregarCuenta OpcionMenuSecundario()
+        {
+
+            //RECURSOS 
+            AgregarCuenta cuenta = AgregarCuenta.Salir;
+            bool opcionCorrecta;
+
+            do
+            {
+                opcionCorrecta = false;
+                //Mostrar Menu de tipo de Cuentas
+                MenuSecundario();
+                //SELECCIONAR EL TIPO DE CUENTA
+                try
+                {
+                    cuenta = (AgregarCuenta)LeerOpcion((byte)AgregarCuenta.Salir, (byte)AgregarCuenta.Cuenta_Platino);
+                    opcionCorrecta = true;
+                }
+                catch (Exception err)
+                {
+                    InformarError(err.Message);
+                }
+
+            } while (!opcionCorrecta);
+
+            return cuenta;
+        }
+
+        public static OperacionesCuenta OpcionMenuOperaciones()
+        {
+            OperacionesCuenta operaciones = OperacionesCuenta.Salir;
+            bool opcionCorrecta;
+
+            do
+            {
+                opcionCorrecta = false;
+
+                //MOSTRAR MENU OPERACIONES
+                MenuOperaciones();
+
+                //SELECCIONAR TIPO DE CUENTAS
+                try
+                {
+                    operaciones = (OperacionesCuenta)LeerOpcion((byte)OperacionesCuenta.Salir, (byte)OperacionesCuenta.Retirar);
+                    opcionCorrecta = true; 
+                }
+                catch(Exception err5)
+                {
+                    InformarError(err5.Message);
+                }
+
+            } while (!opcionCorrecta);
+
+
+            return operaciones;
+        }
+    }
+}
---
+++ GestionBancaria/Program.cs
@@ -0,0 +1,171 @@
+﻿using BGCuentas;
+
+namespace GestionBancaria
+{
+
+    public enum Opcion : byte { Salir, Agregar, Modificar, Eliminar, Consultar, Operar, Cargar }
+
+    public enum AgregarCuenta : byte { Salir, Cuenta_Joven, Cuenta_Oro, Cuenta_Platino }
+
+    public enum OperacionesCuenta : byte { Salir, Ingresar, Retirar }
+
+    internal class Program
+    {
+        /// <summary>
+        /// Controlador principal de la aplicacion
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+            bool salir = false;
+            Opcion opcion = Opcion.Cargar;  //opcion.cargar , como si inicializar un INT a 0 por ejemplo, da igual
+
+            List<Cuenta> listaCuentas = new List<Cuenta>(); //Coleccion de la clase Cuenta(BASE), es decir, dentro de esta coleccion puedes tocar las otras tb
+
+            do
+            {
+                //CAPTAR OPCION DEL MENU PRINCIPAL
+                opcion = Interfaz.OpcionDelMenuPrincipal();
+                
+
+                //EJECUTAR ACCION SEGUN LA OPCION
+                switch (opcion)
+                {
+                    case Opcion.Salir:
+                        //Preguntar confirmacion para salir
+                        salir = true;
+                        break;
+                    case Opcion.Agregar:
+                        AgregarCuenta(listaCuentas);
+                        break;
+                    case Opcion.Modificar:
+                        ModificarCuenta();
+                        break;
+                    case Opcion.Eliminar:
+                        EliminarCuenta();
+                        break;
+                    case Opcion.Consultar:
+                        ConsultarCuenta();
+                        break;
+                    case Opcion.Operar:
+                        OperarCuenta();
+                        break;
+                    case Opcion.Cargar:
+                        CargarCuenta();
+                        break;
+                }
+
+            } while (!salir);
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// Controlador secundario: Adicion de cuentas
+        /// </summary>
+        static void AgregarCuenta(List<Cuenta> lista)
+        {
+
+            bool salir = false;
+            AgregarCuenta cuenta = GestionBancaria.AgregarCuenta.Salir;
+            Cuenta cuenta1 = new Cuenta("Pepeeeee");
+
+            do
+            {
+
+                //CAPTAR OPCION MENU SECUNDARIO
+                cuenta = Interfaz.OpcionMenuSecundario();
+                
+
+
+                //EJECUTAR LA OPCION - AGREGAR LA CUENTA
+                switch (cuenta)
+                {
+                    case GestionBancaria.AgregarCuenta.Salir:
+                        salir = true; 
+                        break;
+                    case GestionBancaria.AgregarCuenta.Cuenta_Joven:
+                        lista.Add(cuenta1);
+                        break;
+                    case GestionBancaria.AgregarCuenta.Cuenta_Oro:
+                        break;
+                    case GestionBancaria.AgregarCuenta.Cuenta_Platino:
+                        break; 
+                }
+
+            } while (!salir);
+            
+
+
+
+        }
+
+        static void ModificarCuenta()
+        {
+            //Mostrar Lista de Cuentas Existentes - POLIMORFISMO
+
+            //Seleccionar la cuenta a modificar
+
+            //Ejecutar la opcion - Modificar la cuenta
+        }
+
+        static void EliminarCuenta()
+        {
+            //Mostrar Lista de Cuentas Existentes - POLIMORFISMO
+
+            //Seleccionar la cuenta a eliminar
+
+            //Ejecutar la opcion - Eliminar la cuenta
+        }
+
+        static void ConsultarCuenta()
+        {
+            //Mostrar Lista de Cuentas Existentes - POLIMORFISMO
+
+            //Seleccionar la cuenta a Consultar
+
+            //Ejecutar la opcion - Consultar la cuenta
+        }
+
+        static void OperarCuenta()
+        {
+            bool salir = false; 
+            OperacionesCuenta operaciones = new OperacionesCuenta();
+
+            do
+            {
+
+                operaciones = Interfaz.OpcionMenuOperaciones();
+
+                //EJECUTAR LA OPCION - OPERAR EN LA CUENTA
+                switch (operaciones)
+                {
+                    case OperacionesCuenta.Salir:
+                        salir = true;
+                        break;
+                    case OperacionesCuenta.Ingresar:
+                        break;
+                    case OperacionesCuenta.Retirar:
+                        break; 
+                }
+
+
+            } while (!salir);
+        }
+
+        static void CargarCuenta()
+        {
+            //Mostrar Lista de Cuentas Existentes - POLIMORFISMO
+
+            //Seleccionar la cuenta a Cargar
+
+            //Ejecutar la opcion - Cargar la cuenta
+        }
+
+    }
+}
